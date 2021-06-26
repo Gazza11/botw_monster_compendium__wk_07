281582d6ac1a16a2e581f859236087bb56e10621
Gary Darnell <gdarnell9@gmail.com>
2021-06-26 15:54:56 +0100

    Added search by name that updates in real-time.

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -1,8 +1,35 @@
-const Filter = () => {
+import {useState, useEffect} from "react"
+
+const Filter = ({filter}) => {
+
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const changeSearchTerm = (event) => {
+        event.preventDefault()
+        setSearchTerm(event.target.value)
+    }
+
+    useEffect(() => {
+        filter(searchTerm)
+    }, [searchTerm])
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+
     return(
-        <form onSubmit='' className='filter-bar'>
-            <input type='text' name='search' placeholder='Filter by Name'></input>
-        </form>
+        <div className='form-container'>
+        {/* //     <select>
+        //         <option value=''>Select a region to filter by</option>
+        //         <option>Hyrule Field</option>
+        //         <option>East Necula</option>
+        //     </select> */}
+            <form onSubmit={handleSubmit}>
+                <input type='text' name='search' placeholder='Filter by Name' onChange={changeSearchTerm}></input>
+                {/* <input type='submit' value='Submit'></input> */}
+            </form>
+            </div>
     )
 }
 
--- src/container/MonsterContainer.js
+++ src/container/MonsterContainer.js
@@ -7,7 +7,8 @@ import Filter from '../component/Filter';
 const MonsterContainer = () => {
 
     const [monsters, setMonsters] = useState([])
-    const [selectedMonster, setSelectedMosnter] = useState(null)
+    const [selectedMonster, setSelectedMonster] = useState(null)
+    const [filteredMonsters, setFilteredMonsters] = useState(null)
 
     useEffect(() => {
         getMonsters()
@@ -16,23 +17,37 @@ const MonsterContainer = () => {
     const getMonsters = function () {
         fetch('https://botw-compendium.herokuapp.com/api/v1')
         .then(result => result.json())
-        .then(monsters => setMonsters(monsters.data.monsters))
+        .then(monsters => {
+            setMonsters(monsters.data.monsters)
+            setFilteredMonsters(monsters.data.monsters)
+                    })
     }
 
     const onMonsterClick = function (monster){
-        setSelectedMosnter(monster)
+        setSelectedMonster(monster)
         if (selectedMonster === monster) {
-            setSelectedMosnter(null)
+            setSelectedMonster(null)
         }
     }
 
+    const filter = (searchTerm) => {
+        const prepareSearch = searchTerm
+        console.log(prepareSearch)
+        const filteredMonsters = monsters.filter((monster) => {
+            // console.log('Monster', monster.name)
+            return monster.name.indexOf(prepareSearch) > -1
+        })
+        setFilteredMonsters(filteredMonsters)
+        console.log(filteredMonsters)
+
+    }
 
     return(
         <div className='like-body'>
             <Header />
-            <Filter />
+            <Filter filter={filter}/>
             <div className='main-container'>
-            <MonsterList monsters={monsters} onMonsterClick={onMonsterClick}/>
+            <MonsterList monsters={filteredMonsters} onMonsterClick={onMonsterClick}/>
             {selectedMonster ? <MonsterDetail selectedMonster={selectedMonster} /> : null}
             </div>
         </div>
